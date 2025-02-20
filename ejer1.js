2779dbe985e96a87105054cd2043b443129e6336
JSBHA <jhoan-071995@hotmail.com>
2025-02-20 15:42:10 -0500

    Challenge 1 is completed, duplicate gifts are removed and the list is submitted in order

--- ejer1.js
+++ ejer1.js
@@ -1,21 +1,20 @@
 function prepareGifts(gifts) {
-    
-      // Elimina los duplicados usando un Set y luego convierte el Set en un array nuevamente
-    let uniqueGifts = [...new Set(gifts)];
+  // Elimina los duplicados usando un Set y luego convierte el Set en un array nuevamente
+  let uniqueGifts = [...new Set(gifts)];
 
-    // Ordena los regalos en orden ascendente
-    let orderGifts = uniqueGifts.sort(function (a, b) {
-        return a - b; // Compara los valores numéricos para ordenarlos de menor a mayor
-      });
+  // Ordena los regalos en orden ascendente
+  let orderGifts = uniqueGifts.sort(function (a, b) {
+    return a - b; // Compara los valores numéricos para ordenarlos de menor a mayor
+  });
 
-    return orderGifts  // Devuelve los regalos ordenados sin duplicados     
-  }
+  return orderGifts; // Devuelve los regalos ordenados sin duplicados
+}
 
-  const gifts1 = [3, 1, 2, 3, 4, 2, 5]
-  const preparedGifts1 = prepareGifts(gifts1)
+const gifts1 = [3, 1, 2, 3, 4, 2, 5];
+console.log(prepareGifts(gifts1));
 
-  const gifts2 = [6, 5, 5, 5, 5]
-  const preparedGifts2 = prepareGifts(gifts2)
+const gifts2 = [6, 5, 5, 5, 5];
+console.log(prepareGifts(gifts2));
 
-  const gifts3 = []
-  const preparedGifts3 = prepareGifts(gifts3)
+const gifts3 = [];
+console.log(prepareGifts(gifts3));
