11e405fe6484b1c5aaf1b0f7083579a070113640
JSBHA <jhoan-071995@hotmail.com>
2025-02-21 13:43:54 -0500

    Challenge 2 is carried out. We use methods to find the longest length, also to fill spaces and create a frame returning the strings.

--- ejer2.js
+++ ejer2.js
@@ -1,20 +1,18 @@
-
 function createFrame(names) {
   // Paso 1: Encontrar el largo del nombre más largo
-  let maxLength = Math.max(...names.map(name => name.length));
+  let maxLength = Math.max(...names.map((name) => name.length));
 
   // Paso 2: Construir el borde superior e inferior
-  let border = '*'.repeat(maxLength + 2 + 2);
+  let border = "*".repeat(maxLength + 2 + 2);
 
   // Paso 3: Construir las líneas con los nombres
-  let frameName = names.map(name => {
-        return '* ' + name.padEnd(maxLength) + ' *';  
+  let frameName = names.map((name) => {
+    return "* " + name.padEnd(maxLength) + " *";
   });
 
   // Paso 4: Combinar todo
-    return [border, ...frameName, border].join('\n')
-    
-  }
+  return [border, ...frameName, border].join("\n");
+}
 
-  createFrame(['midu', 'madeval', 'educalvolpz'])
-  console.log(createFrame(['midu', 'madeval', 'educalvolpz']));
+createFrame(["midu", "madeval", "educalvolpz"]);
+console.log(createFrame(["midu", "madeval", "educalvolpz"]));
